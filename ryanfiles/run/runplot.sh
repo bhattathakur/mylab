393c21c49391c1e614159a7816ac3f627ba397de
bhattathakur <bhattathaur2015@gmail.com>
2021-09-29 01:08:49 -0400

    creted the data files for corresponding root files from the simulation

--- ryanfiles/run/runplot.sh
+++
@@ -1,4 +0,0 @@
-python3 plot.py co60-1e8.root  co60.gdf  co60.dat
-python3 plot.py cs137-1e8.root cs137.gdf cs137.dat
-python3 plot.py na22-1e8.root  na22.gdf  na22.dat
-python3 plot.py ra226-1e8.root ra226.gdf ra226.dat
---
+++ ryanfiles/shfiles/plot.sh
@@ -0,0 +1,9 @@
+#python3 ~/mylab/ryanfiles/macros/plot.py ~/mylab/ryanfiles/sim_root_files/na22.root ~/mylab/ryanfiles/gdf/na22.gdf ~/mylab/ryanfiles/datfiles/na22.dat
+#python3 ~/mylab/ryanfiles/macros/plot.py ~/mylab/ryanfiles/sim_root_files/co60.root ~/mylab/ryanfiles/gdf/co60.gdf ~/mylab/ryanfiles/datfiles/co60.dat
+#python3 ~/mylab/ryanfiles/macros/plot.py ~/mylab/ryanfiles/sim_root_files/ba133.root ~/mylab/ryanfiles/gdf/ba133.gdf ~/mylab/ryanfiles/datfiles/ba133.dat
+python3 ~/mylab/ryanfiles/macros/plot.py ~/mylab/ryanfiles/sim_root_files/cs137.root ~/mylab/ryanfiles/gdf/cs137.gdf ~/mylab/ryanfiles/datfiles/cs137.dat
+#python3 ~/mylab/ryanfiles/macros/plot.py ~/mylab/ryanfiles/sim_root_files/eu152.root ~/mylab/ryanfiles/gdf/eu152.gdf ~/mylab/ryanfiles/datfiles/eu152.dat
+#python3 ~/mylab/ryanfiles/macros/plot.py ~/mylab/ryanfiles/sim_root_files/pb210.root ~/mylab/ryanfiles/gdf/pb210.gdf ~/mylab/ryanfiles/datfiles/pb210.dat
+#python3 ~/mylab/ryanfiles/macros/plot.py ~/mylab/ryanfiles/sim_root_files/ra226.root ~/mylab/ryanfiles/gdf/ra226.gdf ~/mylab/ryanfiles/datfiles/ra226.dat
+#python3 ~/mylab/ryanfiles/macros/plot.py ~/mylab/ryanfiles/sim_root_files/am241.root ~/mylab/ryanfiles/gdf/am241.gdf ~/mylab/ryanfiles/datfiles/am241.dat
+
